a6d09e952fed9038e7df6e31c6f701aabf408e0c
Astero-H <ahhennane@gmail.com>
2019-01-27 19:06:38 +0100

    first commit:basics

---
+++ src/components/movieitem.vue
@@ -0,0 +1,20 @@
+<template>
+  <li>
+    <h2>{{movie.title}} ({{movie.year}})</h2>
+    <span>{{movie.synopsys}}</span><br>
+    <button @click="$emit('edit', movie)"> Edit </button>
+    <button @click="$emit('remove')"> Remove </button>
+  </li>
+</template>
+
+
+
+<script>
+export default {
+  props: {
+    movie: {
+      type: Object
+    }
+  }
+}
+</script>
